c730b73666fa0d47bae734cee7fc7784ea9207f6
Julio_Leme <julioromao3@gmail.com>
2018-12-21 17:58:35 -0200

    Cadastro de clientes fornecedores e produtos MaxScalla - v1.0

---
+++ src/br/com/maxscalla/ClienteMaxScallaVO.java
@@ -0,0 +1,14 @@
+package br.com.maxscalla;
+
+/**
+ * @author julio.leme@amazonberryacai.com.br
+ * @since {date}
+ * @version 1.0
+ *
+ */
+public class ClienteMaxScallaVO {
+	
+//	Fantasia	Razão Social	Pessoa Física/Juridica	CNPJ 	Inscrição Social	CPF	RG	Telefone Fixo	Telefone Celular	Endereço	Número 	Bairro	Cidade	Estado	CEP	Complemento do endereço
+
+
+}
---
+++ src/br/com/maxscalla/FornecedorMaxScallaVO.java
@@ -0,0 +1,12 @@
+package br.com.maxscalla;
+
+/**
+ * @author julio.leme@amazonberryacai.com.br
+ * @since {date}
+ * @version 1.0
+ *
+ */
+public class FornecedorMaxScallaVO {
+
+	
+}
